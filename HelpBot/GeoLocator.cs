433c7b4780369af4fe127c199a6229f45c7adda0
Daniel Pfeller <pfeller.daniel@gmail.com>
2016-05-02 12:43:40 +0200

    fix geo

--- HelpBot/GeoLocator.cs
+++ HelpBot/GeoLocator.cs
@@ -15,7 +15,7 @@ namespace HelpBot
         {
             //2da7d59b916ec038bdb243d2adf389f4958d5f8e9fe8cf6fb838d72cef829bbf
 
-            string s = new WebClient().DownloadString("http://api.ipinfodb.com/v3/ip-city/?key=2da7d59b916ec038bdb243d2adf389f4958d5f8e9fe8cf6fb838d72cef829bbf&ip="+GetIPAddress() + "&format=json");
+            string s = new WebClient().DownloadString("http://api.ipinfodb.com/v3/ip-city/?key=2da7d59b916ec038bdb243d2adf389f4958d5f8e9fe8cf6fb838d72cef829bbf&format=json");
             dynamic location = JObject.Parse(s);
             return location;
         }
